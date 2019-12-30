5debd20aa6f6f9abfc39217adc16d3e33e27a3c2
alisabaj <alisabaj@gmail.com>
2019-12-30 14:56:53 -0800

    user can browse other users' favorites and add them to their own list

--- src/components/BrowseFavorites.js
+++ src/components/BrowseFavorites.js
@@ -1,10 +1,59 @@
 import React, { Component } from "react";
 
 class Favorites extends Component {
+  constructor() {
+    super();
+    this.state = {
+      favorites: []
+    };
+  }
+  addToOwnFavorites = candidateId => {
+    fetch("http://localhost:4000/favorites", {
+      method: "POST",
+      headers: {
+        Accept: "application/json",
+        "Content-Type": "application/json"
+      },
+      body: JSON.stringify({
+        user_id: this.props.userId,
+        candidate_id: candidateId
+      })
+    })
+      .then(resp => resp.json())
+      .then(json => console.log(json));
+  };
+
+  renderAllFavorites = () => {
+    return this.state.favorites.map((favorite, index) => {
+      return (
+        <div key={index}>
+          <h2>{favorite.candidate.cand_name}</h2>
+          <h3>{favorite.candidate.cid}</h3>
+          <h3>Added by user: {favorite.user_id}</h3>
+          {favorite.user_id !== this.props.userId ? (
+            <button
+              onClick={() => {
+                this.addToOwnFavorites(favorite.candidate_id);
+              }}
+            >
+              Add to Your Favorites
+            </button>
+          ) : null}
+          <br></br>
+        </div>
+      );
+    });
+  };
+  componentDidMount() {
+    fetch("http://localhost:4000/favorites")
+      .then(resp => resp.json())
+      .then(favorites => this.setState({ favorites: favorites }));
+  }
   render() {
     return (
       <div>
-        <h2>Favorites</h2>
+        <h1>Favorites</h1>
+        {this.renderAllFavorites()}
       </div>
     );
   }
--- src/containers/App.js
+++ src/containers/App.js
@@ -64,7 +64,12 @@ class App extends React.Component {
             <Route
               exact
               path="/browsefavorites"
-              component={BrowseFavorites}
+              render={props => (
+                <BrowseFavorites
+                  {...props}
+                  userId={this.state.loggedInId}
+                ></BrowseFavorites>
+              )}
             ></Route>
           </div>
         </Router>
