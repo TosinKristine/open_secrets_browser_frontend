75d556e0ea1de0bcf9b54720ffe85d4e5f195a5b
alisabaj <alisabaj@gmail.com>
2019-12-31 13:08:19 -0800

    can add favorites to your own list if you haven't already favorited it

--- src/components/BrowseFavorites.js
+++ src/components/BrowseFavorites.js
@@ -5,18 +5,9 @@ class Favorites extends Component {
     super();
     this.state = {
       favorites: [],
-      renderedFavorites: []
+      postedFavorites: []
     };
   }
-
-  searchLoggedInFavorites = () => {
-    let loggedFavorites = this.props.userFavorites.map(favorite => {
-      console.log(this.props.userFavorites);
-      return favorite.cid;
-    });
-    console.log("logged favorites..." + loggedFavorites);
-    return loggedFavorites;
-  };
 
   addToOwnFavorites = candidateId => {
     fetch("http://localhost:4000/favorites", {
@@ -34,60 +25,45 @@ class Favorites extends Component {
       .then(json => console.log(json));
   };
 
-  groupFavorites = () => {
-    let groupedFavorites = [];
-    let renderedFavorites = [];
+  componentDidMount() {
+    fetch("http://localhost:4000/favorites")
+      .then(resp => resp.json())
+      .then(favorites => {
+        this.setState({ favorites: favorites });
+        this.organizeFavorites();
+      });
+  }
+
+  organizeFavorites = () => {
+    let newFavorites = [];
+    let organizedFavorites = [];
     this.state.favorites.map(favorite => {
-      if (!groupedFavorites.includes(favorite.candidate.cand_name)) {
-        groupedFavorites.push(favorite.candidate.cand_name);
-        renderedFavorites.push({
+      if (!newFavorites.includes(favorite.candidate.id)) {
+        newFavorites.push(favorite.candidate.id);
+        organizedFavorites.push({
+          candidate_id: favorite.candidate.id,
           candidate_name: favorite.candidate.cand_name,
-          cid: favorite.candidate.cid
+          cid: favorite.candidate.cid,
+          users_favorited: [favorite.user_id]
+        });
+      } else {
+        organizedFavorites.forEach(candidate => {
+          if (candidate.candidate_id === favorite.candidate.id) {
+            candidate.users_favorited.push(favorite.user_id);
+          }
         });
       }
     });
-
-    // console.log(groupedFavorites);
-    // console.log(renderedFavorites);
-    this.setState({ renderedFavorites: renderedFavorites });
+    this.setState({ postedFavorites: organizedFavorites });
   };
 
-  renderGroupedFavorites = () => {
-    return this.state.renderedFavorites.map((favorite, index) => {
+  postedFavorites = () => {
+    return this.state.postedFavorites.map((favorite, index) => {
       return (
         <div key={index}>
           <h2>Candidate: {favorite.candidate_name}</h2>
           <h3>CID: {favorite.cid}</h3>
-          {this.searchLoggedInFavorites()}
-          <button
-            onClick={() => {
-              this.addToOwnFavorites(favorite.candidate_id);
-            }}
-          >
-            Add to Your Favorites
-          </button>
-
-          <br></br>
-        </div>
-      );
-    });
-    // return this.state.groupedFavorites.map((favorite, index) => {
-    //   return (
-    //     <div key={index}>
-    //       <h2>{favorite}</h2>
-    //     </div>
-    //   );
-    // });
-  };
-
-  renderAllFavorites = () => {
-    return this.state.favorites.map((favorite, index) => {
-      return (
-        <div key={index}>
-          <h2>{favorite.candidate.cand_name}</h2>
-          <h3>{favorite.candidate.cid}</h3>
-          <h3>Added by user: {favorite.user_id}</h3>
-          {favorite.user_id !== this.props.userId ? (
+          {!favorite.users_favorited.includes(this.props.userId) ? (
             <button
               onClick={() => {
                 this.addToOwnFavorites(favorite.candidate_id);
@@ -96,53 +72,20 @@ class Favorites extends Component {
               Add to Your Favorites
             </button>
           ) : null}
+
           <br></br>
         </div>
       );
     });
   };
-  componentDidMount() {
-    fetch("http://localhost:4000/favorites")
-      .then(resp => resp.json())
-      .then(favorites => {
-        this.setState({ favorites: favorites });
-        // this.groupFavorites();
-      });
-  }
 
-  organizeFavorites = () => {
-    let newFavorites = [];
-    let newFavoritesArray = [];
-    this.state.favorites.map(favorite => {
-      if (!newFavorites.includes(favorite.candidate.id)) {
-        newFavorites.push(favorite.candidate.id);
-        newFavoritesArray.push({
-          candidate_id: favorite.candidate.id,
-          candidate_name: favorite.candidate.cand_name,
-          cid: favorite.candidate.cid,
-          users_favorited: [favorite.user_id]
-        });
-      } else {
-        newFavoritesArray.forEach(candidate => {
-          if (candidate.candidate_id === favorite.candidate.id) {
-            candidate.users_favorited.push(favorite.user_id);
-          }
-          console.log(candidate.candidate_id);
-        });
-      }
-    });
-    console.log(newFavorites);
-    console.log(newFavoritesArray);
-  };
   render() {
     return (
       <div>
         <h1>Favorites</h1>
-        {/* {this.renderAllFavorites()} */}
-        {/* {this.state.renderedFavorites.length !== 0
-          ? this.renderGroupedFavorites()
-          : null} */}
-        {this.organizeFavorites()}
+        {this.state.postedFavorites.length !== 0
+          ? this.postedFavorites()
+          : null}
       </div>
     );
   }
