7368fa9c73fcdcfc42d7de1e4ed5cc4b32558e6d
alisabaj <alisabaj@gmail.com>
2020-01-28 16:06:44 -0800

    Updates

---
+++ precache-manifest.153f7eb67542458e2f6c5e30618583c8.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "eebea2dd7e35eba946811a79d3d2a045",
+    "url": "/open_secrets_browser_frontend/index.html"
+  },
+  {
+    "revision": "51723dafc4abce0e3aef",
+    "url": "/open_secrets_browser_frontend/static/css/main.45a8b9ca.chunk.css"
+  },
+  {
+    "revision": "b6c7076910c9bcf45922",
+    "url": "/open_secrets_browser_frontend/static/js/2.9b9057f1.chunk.js"
+  },
+  {
+    "revision": "f8513fb9f2981a1b206fb733b985f272",
+    "url": "/open_secrets_browser_frontend/static/js/2.9b9057f1.chunk.js.LICENSE"
+  },
+  {
+    "revision": "51723dafc4abce0e3aef",
+    "url": "/open_secrets_browser_frontend/static/js/main.2d3ecea1.chunk.js"
+  },
+  {
+    "revision": "84cee33800775d158e4a",
+    "url": "/open_secrets_browser_frontend/static/js/runtime-main.7f6f80da.js"
+  }
+]);
